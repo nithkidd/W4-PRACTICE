cb8939053f2f60971eeb13fa2c2ea6c2cddd3cfe
nithkidd <123559135+nithkidd@users.noreply.github.com>
2026-02-21 19:55:24 +0700

    TASK-003 – Downloader – Downloads

--- lib/2_download_app/ui/screens/downloads/downloads_screen.dart
+++ lib/2_download_app/ui/screens/downloads/downloads_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:w4practice/2_download_app/ui/screens/downloads/widgets/download_tile.dart';
 import '../../providers/theme_color_provider.dart';
 import '../../theme/theme.dart';
 import 'widgets/download_controler.dart';
-
 
 class DownloadsScreen extends StatelessWidget {
   // Create the list of fake ressources
@@ -38,7 +38,13 @@ class DownloadsScreen extends StatelessWidget {
 
           SizedBox(height: 50),
 
-          // TODO - Add the Download tiles
+          Expanded(
+            child: ListView.builder(
+              itemCount: ressources.length,
+              itemBuilder: (context, index) =>
+                  DownloadTile(controller: controllers[index]),
+            ),
+          ),
         ],
       ),
     );
--- lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
- 
+
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
   const DownloadTile({super.key, required this.controller});
 
   final DownloadController controller;
- 
- // TODO
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
-    // TODO
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, child) {
+        final status = controller.status;
+        final progress = controller.progress;
+        final size = controller.ressource.size;
+        //Trailing icon when not yet downloaded, downloading, and downloaded, with & without press-able button
+        final trailing = status == DownloadStatus.notDownloaded
+            ? IconButton(
+                icon: const Icon(Icons.download),
+                onPressed: controller.startDownload,
+              )
+            : status == DownloadStatus.downloading
+            ? const Icon(Icons.downloading_outlined)
+            : const Icon(Icons.folder);
+
+        return Container(
+          padding: EdgeInsets.all(8.0),
+          margin: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: ListTile(
+            title: Text(controller.ressource.name),
+            subtitle: Text(
+              '${(progress * 100).toStringAsFixed(0)}% completed - ${(progress * size).toStringAsFixed(1)} MB of $size MB',
+            ),
+            trailing: trailing,
+          ),
+        );
+      },
+    );
   }
 }
